cd5bbb3a40889046320584b17c7937b3969a6036
Luud Janssen <luud-@live.nl>
2023-04-05 18:07:23 +0200

    feat: add links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,26 @@
+import Link from "next/link";
+
 export const metadata = {
-  title: 'Reproduction example',
-}
+  title: "Reproduction example",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <p>
+        <Link href="/">Go to home page</Link>
+      </p>
+      <p>
+        <Link href="/page-source/">Go to destination page</Link>
+      </p>
+      <p>
+        <Link href="/test/">Go to slug page</Link>
+      </p>
     </html>
-  )
+  );
 }
